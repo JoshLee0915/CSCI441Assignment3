f5a5f6c6ac66acebf0bde52756c9490fccc3b092
JoshLee0915 <jlee1@mtech.edu>
2016-03-09 08:54:14 -0700

    added the ability to pass scale, translation, and a view matrix to the SquareVertexShader.glsl

--- CSCI441Assignment2/SquareVertexShader.glsl
+++ CSCI441Assignment2/SquareVertexShader.glsl
@@ -1,20 +1,25 @@
 #version 430 core
+
+uniform mat4 scale;
+uniform mat4 trans;
+uniform mat4 viewMatrix;
+
 out vec4 vertexColor;
 
 void main(void)
 {
 	// rot matrix
-	const mat4 rotY = mat4(
-	vec4( 0.7071, 0, 0.7071, 0),
-	vec4( 0, 1, 0, 0),
-	vec4( -0.7071, 0, 0.7071, 0),
-	vec4( 0, 0, 0, 1 ));
+	//const mat4 rotY = mat4(
+	//vec4( 0.7071, 0, 0.7071, 0),
+	//vec4( 0, 1, 0, 0),
+	//vec4( -0.7071, 0, 0.7071, 0),
+	//vec4( 0, 0, 0, 1 ));
 	
-	const mat4 rotX = mat4(
-	vec4( 1, 0, 0, 0),
-	vec4( 0, 0.7071, -0.7071, 0),
-	vec4( 0, 0.7071, 0.7071, 0),
-	vec4( 0, 0, 0, 1 ));
+	//const mat4 rotX = mat4(
+	//vec4( 1, 0, 0, 0),
+	//vec4( 0, 0.7071, -0.7071, 0),
+	//vec4( 0, 0.7071, 0.7071, 0),
+	//vec4( 0, 0, 0, 1 ));
 	
 	// sides of the cube
 	const vec4 vertices[24] = vec4[24](
@@ -57,8 +62,8 @@ void main(void)
 	vec4(0.0, 0.0, 1.0, 1.0),	// blue
 	vec4(1.0, 0.0, 1.0, 1.0));	// magenta
 	
-	mat4 rot = rotY*rotX;
+	mat4 tsMatrix = trans*scale
 	
-	gl_Position = rot*vertices[gl_VertexID];
+	gl_Position = viewMatrix*tsMatrix*vertices[gl_VertexID];
 	vertexColor = cubeColor[gl_VertexID/4];
 }
